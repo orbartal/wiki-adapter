f2c9ae2dbf66add364ba6523409fd255eba104d8
orbartal <orbartal@gmail.com>
2017-08-10 00:14:42 -0400

    Enable uses of swagger-ui.html and v2/api-docs.

--- servers/java/wiki.adapter.spring.boot/src/main/java/wiki/adapter/spring/boot/controllers/classes/AbstractCrudControllerC.java
+++ servers/java/wiki.adapter.spring.boot/src/main/java/wiki/adapter/spring/boot/controllers/classes/AbstractCrudControllerC.java
@@ -12,13 +12,13 @@ import wiki.adapter.spring.boot.controllers.interfaces.CrudControllerI;
 public abstract class AbstractCrudControllerC <T> implements CrudControllerI<T> {
 
 	@ApiOperation(value = "crud: get all")
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
 	public List<T> get() throws Exception {
 		return null;
 	}
 
 	@ApiOperation(value = "crud: get entity by id")
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET, produces = "application/json")
 	public T get(
 				@ApiParam(value = "The entity id", required = true) @PathVariable("id") Long id
 				) throws Exception {
@@ -26,24 +26,24 @@ public abstract class AbstractCrudControllerC <T> implements CrudControllerI<T> 
 	}
 
 	@ApiOperation(value = "crud: create new entitiy")
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST, produces = "application/json")
 	public T create(
 				@ApiParam(value = "The entity data", required = true) @RequestBody T data
 			) throws Exception {
-		return null;
+		return data;
 	}
 	
 	@ApiOperation(value = "crud: update existing entitiy by id")
-	@RequestMapping(method=RequestMethod.PUT)
+	@RequestMapping(method=RequestMethod.PUT, produces = "application/json")
 	public T update (
 				@ApiParam(value = "The entity id", required = true) @PathVariable Long id, 
 				@ApiParam(value = "The entity data", required = true) @RequestBody T data
 			) throws Exception {
-		return null;
+		return data;
 	}
 
 	@ApiOperation(value = "crud: delete existing entitiy by id")
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE, produces = "application/json")
 	public void delete (
 				@ApiParam(value = "The entity id", required = true) @PathVariable  Long id
 			) throws Exception
--- servers/java/wiki.adapter.spring.boot/src/main/java/wiki/adapter/spring/boot/model/interfaces/WikiCEI.java
+++ servers/java/wiki.adapter.spring.boot/src/main/java/wiki/adapter/spring/boot/model/interfaces/WikiCEI.java
@@ -1,8 +1,12 @@
 package wiki.adapter.spring.boot.model.interfaces;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import wiki.adapter.spring.boot.model.classes.WikiCEC;
 
+@JsonDeserialize(as = WikiCEC.class)
 @ApiModel(value = "Wiki", description = "The wiki is the top level entity in the hierarchy. It can contain other sub wikis, spaces, users, extensions ...")
 public interface WikiCEI {
 
